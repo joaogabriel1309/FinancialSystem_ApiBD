da659e97a01f245494c11686df6513b28eeb957b
Joao.Gabriel <gabriel.122.gm85@gmail.com>
2023-12-11 22:09:57 -0400

    first commit

---
+++ src/main/java/com/joao/apibd/apiBD/ApiBdApplication.java
@@ -0,0 +1,13 @@
+package com.joao.apibd.apiBD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joao/apibd/apiBD/ApiBdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joao.apibd.apiBD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
